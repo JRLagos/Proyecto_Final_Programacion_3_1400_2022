a30f5e76945eb93be69e7c48fd2d0cd5a10c4c30
JRLagos <jrlagosc@unah.hn>
2022-04-21 10:46:26 -0600

    0.2

--- ProyectoFinal/Datos/Interfaces/IUsuarioRepositorio.cs
+++ ProyectoFinal/Datos/Interfaces/IUsuarioRepositorio.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Modelos;
 
-namespace Datos.Interfaces
+namespace Datos.Interfaces;
+
+public interface IUsuarioRepositorio
 {
-    internal interface IUsuarioRepositorio
-    {
-    }
+    Task<bool> Nuevo(Usuario usuario);
+    Task<bool> Actualizar(Usuario usuario);
+    Task<bool> Eliminar(Usuario usuario);
+    Task<IEnumerable<Usuario>> GetLista();
+    Task<Usuario> GetPorcodigo(string codigo);
 }
--- ProyectoFinal/Datos/Repositorios/UsuarioRepositorio.cs
+++ ProyectoFinal/Datos/Repositorios/UsuarioRepositorio.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using Datos.Interfaces;
+using Modelos;
+using MySql.Data.MySqlClient;
 
-namespace Datos.Repositorios
+namespace Datos.Repositorios;
+
+public class UsuarioRepositorio : IUsuarioRepositorio
 {
-    internal class UsuarioRepositorio
+    private string CadenaConexion;
+
+    public UsuarioRepositorio(string cadenaConexion)
     {
+        CadenaConexion = cadenaConexion;
+    }
+
+    private MySqlConnection Conexion()
+    {
+        return new MySqlConnection(CadenaConexion);
+    }
+
+    public Task<bool> Actualizar(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Eliminar(Usuario usuario)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Usuario>> GetLista()
+    {
+        IEnumerable<Usuario> lista = new List<Usuario>();
+
+        try
+        {
+            using MySqlConnection conexion = Conexion();
+            await conexion.OpenAsync();
+            string sql = "SELECT * FROM usuario;";
+            lista = await conexion.QueryAsync<Usuario>(sql);
+        }
+        catch (Exception)
+        {
+        }
+        return lista;
+    }
+
+
+    public Task<Usuario> GetPorcodigo(string codigo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Nuevo(Usuario usuario)
+    {
+        throw new NotImplementedException();
     }
 }
