7aaf6cf5c9962293dc01fdef4c7889faa173ea13
AndinoJose <jandino820@yahoo.com>
2022-04-21 14:50:35 -0600

    Commit jc
    Commit jc

--- ProyectoFinal/ProyectoFinal/Interfaz/IUsuarioServicio.cs
+++ ProyectoFinal/ProyectoFinal/Interfaz/IUsuarioServicio.cs
@@ -1,12 +1,14 @@
 ﻿using Modelos;
 
-namespace Datos.Interfaces;
+namespace ProyectoFinal.Interfaz;
 
-public interface IUsuarioRepositorio
-{
+public interface IUsuarioServicio
+    {
     Task<bool> Nuevo(Usuario usuario);
     Task<bool> Actualizar(Usuario usuario);
     Task<bool> Eliminar(Usuario usuario);
     Task<IEnumerable<Usuario>> GetLista();
     Task<Usuario> GetPorcodigo(string codigo);
+
 }
+
