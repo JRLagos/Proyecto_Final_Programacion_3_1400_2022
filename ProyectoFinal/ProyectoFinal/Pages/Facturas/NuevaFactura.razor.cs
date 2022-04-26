f450903dfb1bcb17f7a6ae388218aca8e9a0d3e8
maykizaguirre <maykizaguirre4@gmail.com>
2022-04-26 12:10:02 -0600

    Cambio cálculo en la factura
    Cambio

--- ProyectoFinal/ProyectoFinal/Pages/Facturas/NuevaFactura.razor.cs
+++ ProyectoFinal/ProyectoFinal/Pages/Facturas/NuevaFactura.razor.cs
@@ -12,13 +12,19 @@ namespace ProyectoFinal.Pages.Facturas
         [Inject] SweetAlertService Swal { get; set; }
 
         private Factura factura = new Factura();
+        private Automovil automovil = new Automovil();
 
+        
         protected async Task Agregar()
         {
             if (string.IsNullOrEmpty(Convert.ToString(factura.IdFactura)) || string.IsNullOrEmpty(factura.IdentidadCliente) || (string.IsNullOrEmpty(Convert.ToString(factura.Fecha))) || (string.IsNullOrEmpty(Convert.ToString(factura.SubTotal))) || (string.IsNullOrEmpty(Convert.ToString(factura.Total))))
             {
                 return;
             }
+
+            factura.SubTotal = factura.Cantidad * automovil.Precio;
+            factura.Impuesto = factura.SubTotal * 0.15M;
+            factura.Total = factura.SubTotal + factura.Impuesto;
 
             bool inserto = await facturaServicio.InsertarFactura(factura);
             if (inserto)
