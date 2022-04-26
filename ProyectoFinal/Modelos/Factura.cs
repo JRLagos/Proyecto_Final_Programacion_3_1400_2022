c5ea6a6872840ded8a9127e94cf1a1e2308cba52
JRLagos <jrlagosc@unah.hn>
2022-04-25 19:40:23 -0600

    Actualización Jorge Lagos

--- ProyectoFinal/Modelos/Factura.cs
+++ ProyectoFinal/Modelos/Factura.cs
@@ -8,7 +8,10 @@ public class Factura
     public int IdFactura { get; set; }
     [Required(ErrorMessage = "El Campo IdentidadCliente Es Obligatorio")]
     public string IdentidadCliente { get; set; }
+    public string NombreCliente { get; set; }
     public DateTime Fecha { get; set; }
+    public string CodigoAutomovil { get; set; }
+    public int Cantidad { get; set; }
     public decimal SubTotal { get; set; }
     public decimal Impuesto { get; set; }
     public decimal Total { get; set; }
